ab747a1a0fb5b915c838b69eeb4232b864383629
Yurii <152527309+Waitin4Silksong@users.noreply.github.com>
2024-11-01 02:48:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+NetworkInterface[] netintfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+foreach (NetworkInterface nif in netintfaces)
+{
+    Console.WriteLine($"Interface: {nif.Name}");
+    Console.WriteLine($"Description: {nif.Description}");
+    Console.WriteLine($"NI Type: {nif.NetworkInterfaceType}");
+    Console.WriteLine($"Operational Status: {nif.OperationalStatus}");
+    Console.WriteLine($"Speed: {nif.Speed / 1_000_000} Мбіт/с");
+
+    IPv4InterfaceStatistics stats = nif.GetIPv4Statistics();
+    Console.WriteLine($"Bytes Received: {stats.BytesReceived}");
+    Console.WriteLine($"Bytes Sent: {stats.BytesSent}");
+    Console.WriteLine("_____________________________________________");
+}
